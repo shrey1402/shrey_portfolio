ad672ea34c35bd9a06fdf06e76ea9bf536efc688
shrey1402 <shreykatharotiya014@gmail.com>
2025-02-03 14:42:48 +0530

    fix issue

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -6,7 +6,7 @@ export const ContactSection = () => {
   return (
     <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
-      <SectionHeader eyebrow="Contact Us" title="Let's Connect" />
+      <SectionHeader eyebrow="Contact Us" title="Let&apos;s Connect" />
 
         <div className="mt-20 bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
           <div className="absolute inset-0 opacity-5 -z-10 " style={{
@@ -14,9 +14,9 @@ export const ContactSection = () => {
           }}></div>
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
-              <h2 className="font-serif text-2xl md:text-3xl">Let's create something amazing together</h2>
+              <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s create something amazing together</h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
+                Ready to bring your next project to life? Let&apos;s connect and discuss how I can help you achieve your goals.
               </p>
             </div>
             <div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -80,7 +80,7 @@ export const HeroSection = () => {
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming design into functional, high-performing
-            web application. Let's discuss your next project.
+            web application. Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
